868357f40b7ab2fc1022f332384b251028d16926
jampan02 <kusudatu@yahoo.co.jp>
2021-03-10 06:24:41 +0900

    change comment

--- components/head.tsx
+++ components/head.tsx
@@ -20,7 +20,7 @@ const HEAD = (props: Props): JSX.Element => {
     description =
       "英語、筋トレ、IT等幅広いコンテンツを取り扱っております。読めば得すること間違いなしの記事を沢山書いております。よかったら見ていってください。";
   }
-  //urlをこねくと
+  //urlをつなげる
   url = `https://unruffled-spence-0404bb.netlify.app/posts/${url}`;
   return (
     <Head>
