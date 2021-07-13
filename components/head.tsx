04b45ffc16f8d5b0b96599b3a536ee24beccef3a
jampan02 <kusudatu@yahoo.co.jp>
2021-07-14 07:54:14 +0900

    add serch console meta

--- components/head.tsx
+++ components/head.tsx
@@ -29,6 +29,10 @@ const HEAD = (props: Props): JSX.Element => {
     <Head>
       <title>{title ? `${title} | Fronted Code` : "Fronted Code"}</title>
       {/*固定設定 */}
+      <meta
+        name="google-site-verification"
+        content="WfH9eg4kRFDMlJjKCsc_lfBFNuJ-D8Jcvi4LyyumXY0"
+      />
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
