d2f61d2d85437407acf1702735b783e131942eca
jampan02 <kusudatu@yahoo.co.jp>
2021-07-15 07:35:15 +0900

    join path

--- pages/posts/category/[...category].tsx
+++ pages/posts/category/[...category].tsx
@@ -22,7 +22,7 @@ const Category = ({ contents }: { contents: CategoryProps }) => {
       }に関する投稿一覧`}</p>
       {contents.data.map((content: any) => (
         <Link
-          href={`/posts/[category]/${"[miniCategory]/"}[id]`}
+          href={`/posts/[category]/[miniCategory]/[id]`}
           as={`/posts/${path}/${content.id}`}
           key={`${path}/${content.id}atCategory`}
         >
